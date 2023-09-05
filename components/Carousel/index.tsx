330961e3d1bf2b935b893e29f178e89abefee856
Tomas Lillo Sanhueza <108248835+TommiL90@users.noreply.github.com>
2023-09-05 10:42:38 -0300

    feat: set interval

--- components/Carousel/index.tsx
+++ components/Carousel/index.tsx
@@ -44,7 +44,7 @@ const CarouselCards = ({ cardList, autoPlay }: CarouselCArdsProps) => {
     if (autoPlay) {
       const interval = setInterval(() => {
         selectNewImage(selectedIndex, cardList)
-      }, 3000)
+      }, 3300)
       return () => clearInterval(interval)
     }
   })
